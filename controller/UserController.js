2a07365d6adbcda688f601d0d35536ac278d5453
mick243 <kjm3746@gmail.com>
2024-05-22 23:21:04 +0900

    5월 22일 두번째 수정

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,9 @@
 const connection = require('../mysql');
 const {StatusCodes} = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const cryoto = require('crypto');
+const dotenv = require('dotenv');
+dotenv.config();
 // const {body, param, validationResult} = require('express-validator');
 
 
@@ -11,15 +15,22 @@ const join =
 // ]
 // ,
 (req, res) => {
-    if(req.body == {}) {
-        res.status(StatusCodes.BAD_REQUEST).json({
-            message : `입력 값을 다시 확인해주세요.`
-        })
-        } else {
+    // if(req.body == {}) {
+    //     res.status(StatusCodes.BAD_REQUEST).json({
+    //         message : `입력 값을 다시 확인해주세요.`
+    //     })
+    //     } else {
         const {email, password} = req.body;
+        
+        let sql = `INSERT INTO users(email, password, salt) VALUES(?,?,?)`
+        
+        //회원가입 시 비밀번호를 암호화해서 암호화된 비밀번호와, salt 값을 같이 DB에저장
+        const salt = cryoto.randomBytes(10).toString('base64');
+        const hashPassword = cryoto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64')
 
-        let sql = `INSERT INTO users(email, password) VALUES(?,?)`
-        let values = [email, password]
+        
+
+        let values = [email, hashPassword, salt];
         connection.query(sql, values,
             function (err, results) {
                 if(err){
@@ -29,6 +40,107 @@ const join =
                     res.status(StatusCodes.CREATED).json(results);
             })
         }
+// }
+
+const login = (req,res) => {
+    // [
+    //     body('email').notEmpty().isEmail().withMessage('이메일 확인필요'),
+    //     body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
+    //     validate
+    // ]
+        const {email, password} = req.body;
+
+        let sql = `SELECT * FROM users WHERE email = ?`
+
+        connection.query(sql, email,
+            function (err, results) {
+                if(err){
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                
+                const loginUser = results[0];
+
+                const rawPassword = cryoto.pbkdf2Sync(password,loginUser.salt, 10000, 10, 'sha512').toString('base64')
+                
+                if(loginUser && loginUser.password == password) {
+                    
+                        const token = jwt.sign({
+                            email : loginUser.email
+                        }, process.env.PRIVATE_KEY, {
+                            expiresIn : '30m',
+                            issuer : 'Me'
+                        });
+
+                        res.cookie('token', token,{
+                            httpOnly : true
+                        })
+
+                        res.status(StatusCodes.OK).json({
+                            message : "로그인 되었습니다."
+                        })
+                    } else {
+                    res.status(StatusCodes.FORBIDDEN).json({
+                        message : "email또는 pwd가 틀렸습니다."
+                    })
+                }
+            }
+        )
+    }
+
+const resetpw = (req, res) => {
+    const {email, password} = req.body;
+
+        let sql = `SELECT * FROM users WHERE email = ?`
+
+        connection.query(sql, email,
+            function (err, results) {
+                if(err){
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+
+                const user = results[0];
+                if(user) {
+                    return res.status(StatusCodes.OK).json({
+                        email : email
+                    });
+                    } else {
+                    return res.status(StatusCodes.FORBIDDEN).end();
+                }
+            })
+};
+
+const resetpwRequest = (req,res) => {
+    const {email, password} = req.body;
+
+    let sql = 'UPDATE users SET password = ?, salt = ?  WHERE email = ?'
+
+    const salt = cryoto.randomBytes(10).toString('base64');
+    const hashPassword = cryoto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64')
+
+    let values = [hashPassword, salt, email];
+    connection.query(sql, values,
+        function (err, results) {
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if(results.affectedRows == 0){
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }else{
+                res.status(StatusCodes.OK).json({
+                    message : "비밀번호가 변경되었습니다."
+                })
+            }
+        }
+    )  
 }
 
-module.exports = join;
+module.exports = {
+    join, 
+    login, 
+    resetpw,
+    resetpwRequest
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ const {StatusCodes} = require('http-status-codes');
 const router = express.Router();
 const connection = require('../mysql');
 const {body, param, validationResult} = require('express-validator');
-const join = require('../controller/UserController')
+const {
+    join, 
+    login, 
+    resetpw, 
+    resetpwRequest
+} = require('../controller/UserController')
 
 const jwt = require('jsonwebtoken');
 
@@ -19,108 +24,12 @@ const validate = (req, res, next) => {
     }
 };
 
-router.post('/join', join)
+router.post('/join', join);
 
-router.post(
-    '/login', 
-    [
-        body('email').notEmpty().isEmail().withMessage('이메일 확인필요'),
-        body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
-        validate
-    ]
-    ,function(req, res) {
-        const {email, password} = req.body;
+router.post('/login', login);
 
-        let sql = `SELECT * FROM users WHERE email = ?`
+router.post('/resetpw', resetpw);
 
-        connection.query(sql, email,
-            function (err, results) {
-                if(err){
-                    console.log(err);
-                    return res.status(400).end();
-                }
-                
-                var loginUser = results[0];
-
-                if(loginUser && loginUser.password == password) {
-                    
-                        const token = jwt.sign({
-                            email : loginUser.email
-                        }, process.env.PRIVATE_KEY, {
-                            expiresIn : '30m',
-                            issuer : 'Me'
-                        });
-
-                        res.cookie('token', token,{
-                            httpOnly : true
-                        })
-
-                        res.status(200).json({
-                            message : "로그인 되었습니다."
-                        })
-                    } else {
-                    res.status(403).json({
-                        message : "email또는 pwd가 틀렸습니다."
-                    })
-                }
-            }
-        )
-    })
-
-router.post('/resetpw',  
-        [
-        body('email').notEmpty().isEmail().withMessage('이메일 확인필요'),
-        validate
-        ]
-        , function(req, res) {
-        const {email} = req.body;
-
-        let sql = `UPDATE users SET password = '' WHERE email = ?`
-        connection.query(sql, email,
-            function (err, results) {
-                if(err){
-                    console.log(err);
-                    return res.status(400).end();
-                }
-
-                if(results.affectedRows == 0 ){
-                    res.status(403).json({
-                        message : "email이 틀렸습니다."
-                    })
-                } else {
-                    res.status(200).json({
-                        message : "비밀번호가 초기화 되었습니다."
-                    })
-                }
-            })
-    })
-    .put('/resetpw', 
-    [
-        body('email').notEmpty().isEmail().withMessage('이메일 확인필요'),
-        body('password').notEmpty().isString().withMessage('비밀번호를 입력해주세요'),
-        validate
-    ]
-    ,function(req, res){
-        const {email, password} = req.body;
-
-        let sql = `UPDATE users SET password = ? WHERE email = ?`
-        let values = [password,email]
-        connection.query(sql, values,
-            function (err, results) {
-                if(err){
-                    console.log(err);
-                    return res.status(400).end();
-                }
-
-                if(results.affectedRows == 0){
-                    return res.status(400).end();
-                }else{
-                    res.status(200).json({
-                        message : "비밀번호가 변경되었습니다."
-                    })
-                }
-            }
-        )  
-    })
+router.put('/resetpwRequest', resetpwRequest);
 
 module.exports = router;
