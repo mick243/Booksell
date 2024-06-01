587a4aabc055a9c3c18447df13b85d1728267a25
mick243 <kjm3746@gmail.com>
2024-06-01 21:52:56 +0900

    "6월 1일"

--- mysql.js
+++ mysql.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'Booksell',
-  dateStrings: true
-});
+
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'Booksell',
+    dateStrings: true
+  });
 
 module.exports = connection
