d48c0d4a56cc94808d7919911e0c418598011206
mick243 <kjm3746@gmail.com>
2024-06-10 11:02:18 +0900

    6월 10일

--- controller/UserController.js
+++ controller/UserController.js
@@ -23,7 +23,11 @@ const join = (req, res) => {
                     console.log(err)
                     return res.status(StatusCodes.BAD_REQUEST).end();
                 }
+
+                if(results.affectedRows)
                     res.status(StatusCodes.CREATED).json(results);
+                else
+                    return res.status(StatusCodes.BAD_REQUEST).end();
             })
 };
 
